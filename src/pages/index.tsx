344c5abfc6ff7ee09841683ad4e7625ef7b6b798
Davi Roberto <daviroberto11@hotmail.com>
2022-02-25 16:26:42 -0300

    structure end

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Init | drNews</title>
+      </Head>
+
+      <h1>Hello!</h1>
+    </>
+  )
+}
